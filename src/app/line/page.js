a3bdc27971f6f1e8a2dffbe45a2d44cde4c1789c
LongPC <phamlong992k@gmail.com>
2024-04-10 09:02:28 +0700

    no message

--- src/app/line/page.js
+++ src/app/line/page.js
@@ -3,7 +3,7 @@ import ChartGeneration from "@/app/components/ChartGeneration";
 
 export const metadata = {
     title: "Free Online Line Chart Maker | Create Stunning Charts in Minutes",
-    description: "This tool help you make chart easier. Create stunning charts & graphs online in minutes!  Our free chart maker is easy to use and offers a variety of chart types to visualize your data. Perfect for presentations, reports, and even educational projects! (Great for students & teachers!)",
+    description: "This tool help you make chart easier. Create stunning charts & graphs online in minutes!  Our free chart maker is easy to use and offers a variety of chart types to visualize your data. Perfect for presentations, reports, and even educational projects! (Great for students & teachers!). Help you make chart online free and faster. Giúp bạn vẽ biểu đồ đường online nhanh chóng. Biểu đồ đường.",
     keywords:["Chart", "Biểu đồ", "Chart maker", "Chart online", "Line chart"],
     robots:"index,follow"
 };
--- src/app/pie/page.js
+++ src/app/pie/page.js
@@ -4,7 +4,7 @@ import ChartGeneration from "@/app/components/ChartGeneration";
 
 export const metadata = {
     title: "Free Online Line Chart Maker | Create Stunning Charts in Minutes",
-    description: "This tool help you make chart easier. Create stunning charts & graphs online in minutes!  Our free chart maker is easy to use and offers a variety of chart types to visualize your data. Perfect for presentations, reports, and even educational projects! (Great for students & teachers!)",
+    description: "This tool help you make chart easier. Create stunning charts & graphs online in minutes!  Our free chart maker is easy to use and offers a variety of chart types to visualize your data. Perfect for presentations, reports, and even educational projects! (Great for students & teachers!). Giúp bạn vẽ biểu đồ tròn nhanh chóng. Biểu đồ tròn.",
     keywords:["Chart", "Biểu đồ", "Chart maker", "Chart online", "Line chart"],
     robots:"index,follow"
 };
